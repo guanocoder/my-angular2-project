efa09dd5f196ec3c3492deea36e485e52c877798
mikser.vladimirovich <mikser.vladimirovich@yandex.ru>
2017-07-06 00:13:07 +0300

    recipe list with *ngFor using Recipe class as model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Array<Recipe> = [
+    new Recipe('A test recipe', 'This is only a test', 'https://www.cryingoverspiltmilk.co.nz/wp-content/uploads/2015/03/pixabay/b/recipe_1426460443.png'),
+    new Recipe('Recipe for Destruction', 'No idea where this came from', 'http://orig14.deviantart.net/7f56/f/2012/123/7/5/poetry_by_dyavoldesigns696-d4yecet.jpg')
+  ];
 
   constructor() { }
 
