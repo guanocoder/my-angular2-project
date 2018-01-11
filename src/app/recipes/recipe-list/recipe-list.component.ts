da8076a65ec9323e94c2e3966864e6172b8c0991
mikser.vladimirovich <mikser.vladimirovich@yandex.ru>
2018-01-12 02:04:57 +0300

    recipe list bindings, onSelected event bubble

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -8,6 +8,8 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
   recipes: Array<Recipe> = [
     new Recipe('A test recipe', 'This is only a test', 'https://www.cryingoverspiltmilk.co.nz/wp-content/uploads/2015/03/pixabay/b/recipe_1426460443.png'),
     new Recipe('Recipe for Destruction', 'No idea where this came from', 'http://orig14.deviantart.net/7f56/f/2012/123/7/5/poetry_by_dyavoldesigns696-d4yecet.jpg')
@@ -16,6 +18,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  select(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
   }
 
 }
