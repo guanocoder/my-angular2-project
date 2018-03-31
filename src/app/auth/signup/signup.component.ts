3b0f49d678e911c0960bfddb76b96edce423179c
mikser.vladimirovich <mikser.vladimirovich@yandex.ru>
2018-04-01 02:24:20 +0300

    @ngrx/effects Error Handling! got rid of pipe() cryptocrapic code

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 
 import { AppState } from '../../ngrx/app.reducers';
 import { TrySignUpAction } from '../../ngrx/auth.actions';
@@ -9,11 +10,21 @@ import { TrySignUpAction } from '../../ngrx/auth.actions';
     selector: 'app-signup',
     templateUrl: './signup.component.html',
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent implements OnInit, OnDestroy {
+
+    private subscription: Subscription;
+    public errorMessage: string;
 
     constructor(private store: Store<AppState>) { }
 
     ngOnInit() {
+        this.subscription = this.store.select('auth').subscribe(authState => {
+            this.errorMessage = authState.errorMessage;
+        })
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
     }
 
     public onSignUp(form: NgForm) {
--- src/app/ngrx/auth.effects.ts
+++ src/app/ngrx/auth.effects.ts
@@ -1,7 +1,6 @@
 import { Effect, Actions } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { map, switchMap, mergeMap, catchError } from 'rxjs/operators';
 import { AuthActions, TrySignUpAction, TrySignInAction, SignUpAction, SetTokenAction, SignInAction, LogOutAction, ErrorOnSignInAction } from './auth.actions';
 import * as firebase from 'firebase';
 import { Router } from '@angular/router';
@@ -17,48 +16,48 @@ export class AuthEffects {
                 password: action.password
             }
         }).switchMap(credentials => {
-            return firebase.auth().createUserWithEmailAndPassword(credentials.email, credentials.password);
-        }).switchMap(_ => {
-            return firebase.auth().currentUser.getToken()
-        }).mergeMap(token => {
-            return [
-                new SignUpAction(),
-                new SetTokenAction(token)
-            ]
-        }).do(_ => {
-            this.router.navigate(["/"]);
+            return Observable.fromPromise(firebase.auth().createUserWithEmailAndPassword(credentials.email, credentials.password)).switchMap(_ => {
+                return firebase.auth().currentUser.getToken();
+            }).mergeMap(token => {
+                return [
+                    new SignUpAction(),
+                    new SetTokenAction(token)
+                ]
+            }).catch(error => {
+                return [
+                    new ErrorOnSignInAction(error.message)
+                ]
+            });
+        }).do(action => {
+            if(action instanceof SetTokenAction)
+                this.router.navigate(["/"]);
         });
 
     @Effect()
     authSignIn = this.actionsObservable
         .ofType(AuthActions.TrySignIn)
-        // Once error occurs in the observer chain
-        // the whole f*cking thing stops working (unsubscribes)
-        // hence the following piece of crap with piping and strange shit
-        .pipe(
-            map((action: TrySignInAction) => {
-                return {
-                    email: action.email,
-                    password: action.password
-                }
-            }),
-            switchMap(credentials => {
-                return Observable.fromPromise(firebase.auth().signInWithEmailAndPassword(credentials.email, credentials.password).then(_ => {
-                    return firebase.auth().currentUser.getToken();
-                })).pipe(
-                    mergeMap((token: string) => {
-                        this.router.navigate(["/"]);
-                        return [
-                            new SignInAction(),
-                            new SetTokenAction(token)
-                        ]
-                    }),
-                    catchError(error => {
-                        return Observable.of(new ErrorOnSignInAction(error.message));
-                    })
-                );
-            })
-        );
+        .map((action: TrySignInAction) => {
+            return {
+                email: action.email,
+                password: action.password
+            }
+        }).switchMap(credentials => {
+            return Observable.fromPromise(firebase.auth().signInWithEmailAndPassword(credentials.email, credentials.password)).switchMap(_ => {
+                return firebase.auth().currentUser.getToken();
+            }).mergeMap(token => {
+                return [
+                    new SignInAction(),
+                    new SetTokenAction(token)
+                ];
+            }).catch(error => {
+                return [
+                    new ErrorOnSignInAction(error.message)
+                ]
+            });
+        }).do(action => {
+            if(action instanceof SetTokenAction)
+                this.router.navigate(["/"]);
+        });
     
     @Effect()
     authLogOut = this.actionsObservable
